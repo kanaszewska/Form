a08616337c71f612a1edae3eb3a7bec01cdfc6fe
Dominika Dębska <kanaszewskadominika@gmail.com>
2022-11-16 18:02:52 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import LogIn from './LogIn';
-import { BrowserRouter as Router, Routes, NavLink, Route } from 'react-router-dom';
+import React from 'react'
+import LogIn from './LogIn'
+import {
+  BrowserRouter as Router,
+  Routes,
+  NavLink,
+  Route,
+} from 'react-router-dom'
 import './App.css'
-import SignUp from './SignUp';
+import SignUp from './SignUp'
 
 function App() {
   return (
     <Router>
-    <div className="app">
-      <div className='nav'>
-        <nav>
-          <ul className='nav'>
-            <li><NavLink to="">LOG IN</NavLink></li>
-            <li><NavLink to="/registration">SIGN UP</NavLink></li>
-          </ul>
-        </nav>
+      <div className="app">
+        <div className="nav">
+          <nav>
+            <ul className="nav">
+              <li>
+                <NavLink to="/Form">LOG IN</NavLink>
+              </li>
+              <li>
+                <NavLink to="/registration">SIGN UP</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <section className="route">
+          <Routes>
+            <Route path="/Form" element={<LogIn />} />
+            <Route path="/registration" element={<SignUp />} />
+          </Routes>
+        </section>
       </div>
-      <section className='route'>
-      <Routes>
-          <Route path="" element={<LogIn/>} />
-          <Route path="/registration" element={<SignUp/>} />
-       </Routes>
-      </section>
-    </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/LogIn.js
+++ src/LogIn.js
@@ -1,106 +1,97 @@
-import React, { useState } from 'react';
-import './Form.css';
+import React, { useState } from 'react'
+import './Form.css'
 
 const Form = () => {
-    
-    const initialValue = {
-        email: '',
-        password: '',
-        accept: false,
+  const initialValue = {
+    email: '',
+    password: '',
+    accept: false,
+  }
+
+  const [formValues, setFormValues] = useState(initialValue)
+  const [formErrors, setFormErrors] = useState({})
+  const [isSubmit, setIsSubmit] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    setFormErrors(validate(formValues))
+    setIsSubmit(true)
+
+    if (Object.keys(validate(formValues)).length === 0 && isSubmit) {
+      alert('Log in is correct')
     }
+    return
+  }
 
-    const [formValues, setFormValues] = useState(initialValue);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
+  const validate = (values) => {
+    const errors = {}
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
 
-        const handleChange = (e) => {
-            const { name, value } = e.target;
-            setFormValues({...formValues, [name]: value});
-        }
+    if (!values.email) {
+      errors.email = 'The email you entered is incorrect'
+    } else if (!regexEmail.test(values.email)) {
+      errors.email = 'Please provide a valid email address.'
+    }
+    if (!values.password) {
+      errors.password = 'The password you entered is incorrect'
+    }
+    return errors
+  }
 
-        const handleOnSubmit = (e) => {
-            e.preventDefault();
-            setFormErrors(validate(formValues));
-            setIsSubmit(true);
-
-            if (Object.keys(validate(formValues)).length === 0 && isSubmit)
-            {alert("Log in is correct");
-        }
-            return
-            };
-
-    
-        const validate = (values) => {
-            const errors = {}
-            const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-            if (!values.email) {
-                errors.email = "The email you entered is incorrect" 
-            } else if (!regexEmail.test(values.email)){
-                errors.email = "Please provide a valid email address."
-            }
-            if (!values.password) {
-                errors.password = "The password you entered is incorrect" 
-            } 
-            return errors;
-        };
-        
-        return(
-        <div className='container'>
-            <form className='form'>
-            <h1>MEMBER LOGIN</h1>
-            <div className='details'>
-                <label>
-                    Email 
-                </label>
-                    <input 
-                        className='emailInput'
-                        name='email'
-                        placeholder='Enter your name'
-                        onChange={handleChange}
-                        type="email"
-                        value={formValues.email}
-                    />
-            <p>{formErrors.email}</p>
-            </div>
-            <div className='details'>
-                <label>
-                    Hasło 
-                </label>
-                    <input 
-                        className='passwordInput'
-                        name='password'
-                        placeholder='Enter your password'
-                        onChange={handleChange}
-                        type="password"
-                        value={formValues.password}
-                    />
-            <p>{formErrors.password}</p>
-            </div>
-            <div className='accept'>
-                <label>
-                <input 
-                    className='accept'
-                    name='accept'
-                    onChange={handleChange}
-                    type="checkbox"
-                    value={formValues.accept}
-                />Keep me logged in 
-                </label>
-            </div>
-            <div>
-            <div className='button-cointainer'>
-                <button 
-                    className='button'
-                    type="submit"
-                    onClick={handleOnSubmit}
-                    >               
-                LOG IN
-                </button>
-            </div>
-            </div>
-        </form>
+  return (
+    <div className="container">
+      <form className="form">
+        <h1>MEMBER LOGIN</h1>
+        <div className="details">
+          <label>Email</label>
+          <input
+            className="emailInput"
+            name="email"
+            placeholder="Enter your name"
+            onChange={handleChange}
+            type="email"
+            value={formValues.email}
+          />
+          <p>{formErrors.email}</p>
         </div>
-            );
-        };
-export default Form;
+        <div className="details">
+          <label>Hasło</label>
+          <input
+            className="passwordInput"
+            name="password"
+            placeholder="Enter your password"
+            onChange={handleChange}
+            type="password"
+            value={formValues.password}
+          />
+          <p>{formErrors.password}</p>
+        </div>
+        <div className="accept">
+          <label>
+            <input
+              className="accept"
+              name="accept"
+              onChange={handleChange}
+              type="checkbox"
+              value={formValues.accept}
+            />
+            Keep me logged in
+          </label>
+        </div>
+        <div>
+          <div className="button-cointainer">
+            <button className="button" type="submit" onClick={handleOnSubmit}>
+              LOG IN
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+export default Form
